d5bdbbf2d6b7ef4008abf74c5f92e6cfdd39f0ac
Mark Grothe <grothem@gmail.com>
2021-11-16 22:13:15 -0600

    Show stake events in StakerUI

--- packages/vite-app-ts/src/app/routes/main/components/Staker.tsx
+++ packages/vite-app-ts/src/app/routes/main/components/Staker.tsx
@@ -1,7 +1,51 @@
+import { StaticJsonRpcProvider } from '@ethersproject/providers';
+import { TTransactor } from 'eth-components/functions';
+import { useContractLoader, useEventListener } from 'eth-hooks';
+import { useEthersContext } from 'eth-hooks/context';
 import React, { FC } from 'react';
+import { useAppContracts } from '../hooks/useAppContracts';
+import { Staker as StakerContract } from '~~/generated/contract-types';
+import { List } from 'antd';
+import { Address, Balance } from 'eth-components/ant';
+import { formatEther, parseEther } from '@ethersproject/units';
 
-export const Staker: FC = () => (
-  <div>
-    <h1>Staker</h1>
-  </div>
-);
+export interface StakerProps {
+  mainnetProvider: StaticJsonRpcProvider;
+  tx: TTransactor | undefined;
+}
+
+export const Staker: FC<StakerProps> = (props) => {
+  const { mainnetProvider, tx } = props;
+
+  const appContractConfig = useAppContracts();
+  const ethersContext = useEthersContext();
+  const readContracts = useContractLoader(appContractConfig);
+  const writeContracts = useContractLoader(appContractConfig, ethersContext?.signer);
+
+  const yourContractRead = readContracts['Staker'] as StakerContract;
+  const yourContractWrite = writeContracts['Staker'] as StakerContract;
+
+  const stakeEvents = useEventListener(yourContractRead, 'Stake', 1);
+  console.log(stakeEvents);
+
+  return (
+    <div>
+      <h1>Staker</h1>
+      <div style={{ width: 600, margin: 'auto', marginTop: 32, paddingBottom: 32 }}>
+        <h2>Events:</h2>
+        <List
+          bordered
+          dataSource={stakeEvents}
+          renderItem={(item: any) => {
+            return (
+              <List.Item key={item.blockNumber + '_' + item.sender + '_' + item.purpose}>
+                <Address address={item.args[0]} ensProvider={mainnetProvider} fontSize={16} /> =>
+                {formatEther(item.args[1])}
+              </List.Item>
+            );
+          }}
+        />
+      </div>
+    </div>
+  );
+};
