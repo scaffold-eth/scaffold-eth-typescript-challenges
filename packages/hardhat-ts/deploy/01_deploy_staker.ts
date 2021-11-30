35016640af719f2c20ef7a67098294d6f57f2ff6
Mark Grothe <grothem@gmail.com>
2021-11-30 16:19:27 -0600

    Use verify script instead of running verify as part of deploy

--- packages/hardhat-ts/deploy/01_deploy_staker.ts
+++ packages/hardhat-ts/deploy/01_deploy_staker.ts
@@ -1,7 +1,5 @@
-import { ethers, run } from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironmentExtended } from 'helpers/types/hardhat-type-extensions';
-import '@nomiclabs/hardhat-etherscan';
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironmentExtended) => {
   const { getNamedAccounts, getChainId, deployments } = hre as any;
@@ -25,25 +23,6 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironmentExtended) => {
 
     //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
   */
-
-  // todo: uncomment to verify your contract
-  // const Staker = await ethers.getContract('Staker', deployer);
-
-  // if (chainId !== '31337') {
-  //   try {
-  //     console.log(' 🎫 Verifing Contract on Etherscan... ');
-  //     await delay(3000);
-  //     await run('verify:verify', {
-  //       address: Staker.address,
-  //       contract: 'contracts/Staker.sol:Staker',
-  //       constructorArguments: [exampleExternalContract.address],
-  //     });
-  //     console.log(' ✅ Contract Verified! ');
-  //   } catch (e) {
-  //     console.log(' ⚠️ Failed to verify contract on Etherscan ');
-  //     console.log(e);
-  //   }
-  // }
 };
 export default func;
 func.tags = ['Staker'];
