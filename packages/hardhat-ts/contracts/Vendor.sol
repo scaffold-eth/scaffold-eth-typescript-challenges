bd9bdb09b1050f6a9e7a60e0caaab7d59c2a609a
Mark Grothe <grothem@gmail.com>
2021-12-10 21:07:52 -0600

    Getting initial contracts in place

---
+++ packages/hardhat-ts/contracts/Vendor.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.8.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+
+//import "@openzeppelin/contracts/access/Ownable.sol";
+import './YourToken.sol';
+
+contract Vendor {
+  YourToken yourToken;
+
+  constructor(address tokenAddress) public {
+    yourToken = YourToken(tokenAddress);
+  }
+
+  // ToDo: create a payable buyTokens() function:
+
+  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+
+  // ToDo: create a sellTokens() function:
+}
