a786649ffe41a4c25847f084beed58451bd8d7f7
Mark Grothe <grothem@gmail.com>
2021-12-10 21:12:49 -0600

    Setting up deploy scripts

--- packages/hardhat-ts/deploy/00_deploy_your_token.ts
+++ packages/hardhat-ts/deploy/00_deploy_your_token.ts
@@ -6,7 +6,7 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironmentExtended) => {
   const { getNamedAccounts, deployments } = hre as any;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy('YourContract', {
+  await deploy('YourToken', {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
     // args: ["Hello"],
@@ -17,12 +17,12 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironmentExtended) => {
     // Getting a previously deployed contract
     const YourContract = await ethers.getContract("YourContract", deployer);
     await YourContract.setPurpose("Hello");
-    
+
     //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
   */
 };
 export default func;
-func.tags = ['YourContract'];
+func.tags = ['YourToken'];
 
 /*
 Tenderly verification
