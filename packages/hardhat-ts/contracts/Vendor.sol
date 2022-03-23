4311d90d0cf9957b0436b0ab60b3e914b1b4af96
Daniele Salatti <me@danielesalatti.com>
2022-03-23 10:22:29 -0400

    Make yourToken public in Vendor.sol
    Everything works fine except that the autograder tests expect this to be public. Tests in this branch aren't up to date yet and don't work locally (pending #34), this should reduce the number of failed submissions while not detracting from the learning experience.

--- packages/hardhat-ts/contracts/Vendor.sol
+++ packages/hardhat-ts/contracts/Vendor.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import './YourToken.sol';
 
 contract Vendor {
-  YourToken yourToken;
+  YourToken public yourToken;
 
   constructor(address tokenAddress) public {
     yourToken = YourToken(tokenAddress);
