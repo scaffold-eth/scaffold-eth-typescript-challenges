aa6aa8b0ccd52beade32d6651a22466163fe37bd
Sergii Vasylchenko <sergik666@gmail.com>
2022-10-08 19:02:40 +0300

    Added multi sign contract, test and deploy script

---
+++ packages/hardhat-ts/deploy/01_deploy_meta_multi_sig_wallet_contract.ts
@@ -0,0 +1,37 @@
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironmentExtended } from 'helpers/types/hardhat-type-extensions';
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironmentExtended) => {
+  const { getNamedAccounts, deployments } = hre as any;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  await deploy('MetaMultiSigWallet', {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    args: [
+      31337,
+      ["0x53386b18809922960793C4636572e4830F822474"],
+      1
+    ],
+    log: true,
+  });
+
+  /*
+    // Getting a previously deployed contract
+    const YourContract = await ethers.getContract("YourContract", deployer);
+    await YourContract.setPurpose("Hello");
+
+    //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
+  */
+};
+export default func;
+func.tags = ['MetaMultiSigWallet'];
+
+/*
+Tenderly verification
+let verification = await tenderly.verify({
+  name: contractName,
+  address: contractAddress,
+  network: targetNetwork,
+});
+*/
